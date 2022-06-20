7a0c0faccfd476d4b4b44ebd70dc1a1f170412b4
george.facebooka <george.facebooka@gmail.com>
2022-06-20 13:16:26 +0200

    more styles and high score saveing

--- myFirstGame/app/app.js
+++ myFirstGame/app/app.js
@@ -1,19 +1,37 @@
-const song = new Audio("../Alan_Walker_-_Faded.mp3");
+// const song = new Audio("../Alan_Walker_-_Faded.mp3");
 const player = document.querySelector("#player");
 const scoreElement = document.querySelector("#score");
 const startBtn = document.querySelector("#start");
 const startBtnCont = document.querySelector("#startDiv");
 const gameOver = document.querySelector("#gameOver");
+const gameOverText = document.querySelector("#gameOverText");
 const tryAgainBtn = document.querySelector("#tryAgain");
+const sideBar = document.querySelector("#sideBar");
 let score = 0;
-let level = 1;
+let level = 0;
 let speed = 1;
 
 const updateScore = () => {
   score++;
   scoreElement.innerHTML = score;
 };
-
+const sideBarHandler = (time) => {
+  sideBar.style.transition = `height ${time}s ease-in-out`;
+  sideBar.style.height = "100%";
+};
+const levelUp = () => {
+  sideBar.style.transition = `height 0.2s ease-in-out`;
+  sideBar.style.height = "0%";
+  level++;
+  speed += (speed / 100) * 15;
+  const levelUp = document.createElement("div");
+  levelUp.classList.add("levelUp");
+  levelUp.innerHTML = `Level ${level} .. Speed up!`;
+  document.body.appendChild(levelUp);
+  setTimeout(() => {
+    levelUp.remove();
+  }, 1500);
+};
 const playerHandler = (e) => {
   player.style.top = e.clientY + "px";
   player.style.left = e.clientX + "px";
@@ -22,7 +40,6 @@ const playerReady = () => {
   document.addEventListener("click", playerHandler);
   document.addEventListener("pointermove", playerHandler);
 };
-
 const obstacleCrossCheck = () => {
   const playerR = player.getBoundingClientRect().right;
   const playerL = player.getBoundingClientRect().left;
@@ -55,19 +72,10 @@ setInterval(() => {
     }
   });
 }, 100);
-
-const levelUp = () => {
-  level++;
-  speed += (speed / 100) * 15;
-  const levelUp = document.createElement("div");
-  levelUp.classList.add("levelUp");
-  levelUp.innerHTML = `Level ${level}`;
-  document.body.appendChild(levelUp);
-  setTimeout(() => {
-    levelUp.remove();
-  }, 1000);
+const removeObstacles = (e) => {
+  e.target.remove();
+  updateScore();
 };
-
 let n = 0;
 const game = {
   gameLoop: null,
@@ -78,25 +86,39 @@ const game = {
       obstacle.style.animation = `rain ${(1 / speed) * 5}s ease-in-out`;
       obstacle.classList.add("obstacle");
       document.body.appendChild(obstacle);
-      obstacle.addEventListener("animationend", () => {
-        obstacle.remove();
-        updateScore();
-      });
+      obstacle.addEventListener("animationend", removeObstacles);
       n++;
-      if (n % (10 * level) == 0) {
+      if (n >= level * 10.15) {
         levelUp();
+        n = 0;
+        setTimeout(() => {
+          sideBarHandler(Math.ceil(level * 10.15) * 1.5);
+        }, 200);
       }
     }, 1500);
   },
   end: () => {
     clearInterval(game.gameLoop);
+    saveHighScore();
     gameOver.style.display = "flex";
+    gameOverText.innerHTML = `Game Over!<br>Score: ${score} <br>Level: ${level}
+    <br>
+    <br>
+    <div id="highScore">
+    highscore
+    <br>
+    Score: ${JSON.parse(localStorage.getItem("score")).score}
+    <br>
+    Level: ${JSON.parse(localStorage.getItem("score")).level}
+    </div>`;
+
     document.removeEventListener("click", playerHandler);
     document.removeEventListener("pointermove", playerHandler);
     (() => {
       const obstacles = document.querySelectorAll(".obstacle");
       obstacles.forEach((obs) => {
-        obs.remove();
+        obs.removeEventListener("animationend", removeObstacles);
+        obs.style.opacity = "0";
       });
     })();
     n = 0;
@@ -108,7 +130,6 @@ startBtn.addEventListener("click", () => {
     startBtnCont.remove();
     game.start();
     playerReady();
-    song.play();
   });
 });
 tryAgainBtn.addEventListener("click", () => {
@@ -126,3 +147,15 @@ tryAgainBtn.addEventListener("click", () => {
     levelUp();
   }, 1000);
 });
+const saveHighScore = () => {
+  const scoreData = {
+    score: score,
+    level: level,
+  };
+  let currentScore;
+  localStorage.getItem("score")
+    ? (currentScore = JSON.parse(localStorage.getItem("score")))
+    : (currentScore = { score: 0, level: 0 });
+  if (scoreData.score > currentScore.score)
+    localStorage.setItem("score", JSON.stringify(scoreData));
+};
